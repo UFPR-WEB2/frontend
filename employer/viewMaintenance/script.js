7fad5c3b8836ced4bf10ec670f19820e8451fa88
Gabriel Henrique Speraceta <gabriel.hesperanceta@gmail.com>
2024-09-29 20:32:54 -0300

    Refatorar estilo do componente de visualização de manutenção
    Aprimora o estilo do componente de visualização de manutenção, adicionando cores de estado e ajustando o layout para melhorar a legibilidade e usabilidade.

--- employer/viewMaintenance/script.js
+++ employer/viewMaintenance/script.js
@@ -2,23 +2,36 @@ function createProductElement(
 	category,
 	date,
 	description,
-	stateDescription,
-	stateClass
+	stateDescription
 ) {
+	const stateColors = {
+		"ABERTA": "gray",
+		"ORÇADA": "brown",
+		"REJEITADA": "red",
+		"APROVADA": "yellow",
+		"REDIRECIONADA": "purple",
+		"AGUARDANDO PAGAMENTO": "blue",
+		"PAGA": "orange",
+		"FINALIZADA": "green"
+	};
+
+	const stateClass = stateColors[stateDescription] || "default-state-color";
+
 	const productDiv = document.createElement("div");
 	productDiv.className = "product";
 
 	const headerInfoDiv = document.createElement("div");
 	headerInfoDiv.className = "header-info";
 
-	const stateDiv = document.createElement("div");
+	const stateInfoDiv = document.createElement("div");
+	stateInfoDiv.className = "state-info";
 	const stateIndicatorDiv = document.createElement("div");
-	stateIndicatorDiv.className = `state ${stateClass}`;
+	stateIndicatorDiv.className = `state ${stateClass}`; 
 	const stateDescriptionP = document.createElement("p");
 	stateDescriptionP.className = "state-description";
 	stateDescriptionP.textContent = stateDescription;
-	stateDiv.appendChild(stateIndicatorDiv);
-	stateDiv.appendChild(stateDescriptionP);
+	stateInfoDiv.appendChild(stateIndicatorDiv);
+	stateInfoDiv.appendChild(stateDescriptionP);
 
 	const categoryDiv = document.createElement("div");
 	categoryDiv.className = "category";
@@ -42,7 +55,7 @@ function createProductElement(
 	openDropdownButton.className = "open-dropdown";
 	openDropdownButton.textContent = "Abrir";
 
-	headerInfoDiv.appendChild(stateDiv);
+	headerInfoDiv.appendChild(stateInfoDiv);
 	headerInfoDiv.appendChild(categoryDiv);
 	headerInfoDiv.appendChild(dateDiv);
 	headerInfoDiv.appendChild(openDropdownButton);
@@ -56,18 +69,19 @@ function createProductElement(
 	descriptionH3.textContent = "Descrição";
 	const descriptionP = document.createElement("p");
 	descriptionP.textContent = description;
+	descriptionDiv.appendChild(descriptionH3);
+	descriptionDiv.appendChild(descriptionP);
+
 	const redirectStateButton = document.createElement("button");
 	redirectStateButton.className = "redirect-state";
 	redirectStateButton.textContent = stateDescription;
-	descriptionDiv.appendChild(descriptionH3);
-	descriptionDiv.appendChild(descriptionP);
-	descriptionDiv.appendChild(redirectStateButton);
 
 	dropdownPlusDiv.appendChild(descriptionDiv);
+	dropdownPlusDiv.appendChild(redirectStateButton);
 
 	productDiv.appendChild(headerInfoDiv);
 	productDiv.appendChild(dropdownPlusDiv);
 
 	const maintenanceList = document.getElementById("maintenance-list");
-    maintenanceList.appendChild(productDiv);
+	maintenanceList.appendChild(productDiv);
 }
