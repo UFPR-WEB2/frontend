1c887e3f15ddf09b4e8c80ed795b83e264ef2e6e
Gabriel Henrique Speraceta <gabriel.hesperanceta@gmail.com>
2024-09-30 20:32:41 -0300

    Adicao inicial de materiais pro projeto

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -4,17 +4,20 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RouterOutlet } from '@angular/router';
 import { Router } from '@angular/router';
+import { ButtonComponent } from './material';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,  // Defina o AppComponent como standalone
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [HeaderClienteComponent,RouterOutlet]  // Adicione o componente standalone nos imports
+  imports: [HeaderClienteComponent,RouterOutlet, ButtonComponent]  // Adicione o componente standalone nos imports
 })
 export class AppComponent {
   title = 'Meu App';
   constructor(private router: Router) {}
+  
   goToLogin() {
     this.router.navigate(['/login']);
   }
