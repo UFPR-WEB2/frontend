c870306e5da90eb680b323b5d4f9e6805252e48e
Bruno_Brug <brunobrugnerottodelara@gmail.com>
2024-10-10 13:55:06 -0300

    [add] view-equipment-category component

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HeaderClienteComponent } from './material/header-cliente/header-cliente.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ViewEquipmentCategoryComponent } from './pages/employer/view-equipment-category/view-equipment-category.component'; // Importe o componente
+
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
@@ -10,5 +12,6 @@ export const routes: Routes = [
   { path: 'login/register', loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent) },
   { path: 'home-cliente', loadComponent: () => import('./pages/home-cliente/home-cliente.component').then(m => m.HomeClienteComponent) },
   { path: 'view-maintenance', loadComponent: () => import('./pages/employer/view-maintenance/view-maintenance.component').then(m => m.ViewMaintenanceComponent) },
+  { path: 'view-equipment-category', component: ViewEquipmentCategoryComponent },
   { path: '**', redirectTo: '' }
 ];
---
+++ front-end/src/app/pages/employer/view-equipment-category/view-equipment-category.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ViewEquipmentCategoryComponent } from './view-equipment-category.component';
+
+describe('ViewEquipmentCategoryComponent', () => {
+  let component: ViewEquipmentCategoryComponent;
+  let fixture: ComponentFixture<ViewEquipmentCategoryComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ViewEquipmentCategoryComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ViewEquipmentCategoryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
