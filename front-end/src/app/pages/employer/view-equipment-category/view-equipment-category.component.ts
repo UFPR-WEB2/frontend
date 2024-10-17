6969a605d9300ad8b5335645d420e23c35fe2aa3
Bruno_Brug <brunobrugnerottodelara@gmail.com>
2024-10-17 09:22:00 -0300

    rota crud employee

---
+++ front-end/src/app/pages/employer/view-equipment-category/view-equipment-category.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-equipment-category',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-equipment-category.component.html',
+  styleUrl: './view-equipment-category.component.css'
+})
+export class ViewEquipmentCategoryComponent {
+
+}
