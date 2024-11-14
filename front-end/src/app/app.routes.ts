4a2619cd54ac6e375f28cc9b60f6df9d6bea04b6
xXPedrowXx <phlopes646@gmail.com>
2024-11-14 14:18:47 -0300

    [add] authGuard e novas rotas para funcionalidade de funcionário

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
+<<<<<<< Updated upstream
 import { ViewEquipmentCategoryComponent } from './pages/employer/view-equipment-category/view-equipment-category.component';
 
+=======
+import { authGuard } from './auth/auth.guard';
+>>>>>>> Stashed changes
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
@@ -11,7 +15,18 @@ export const routes: Routes = [
   { path: 'cliente/home/orcamento/:id', loadComponent: () => import('./pages/customer/orcamento-cliente/orcamento-cliente.component').then(m => m.OrcamentoClienteComponent) },
   { path: 'cliente/home/pagamento/:id', loadComponent: () => import('./pages/customer/payment/payment.component').then(m => m.PaymentComponent) },
   { path: 'cliente/home/servico/:id', loadComponent: () => import('./pages/customer/view-solicitacao/view-solicitacao.component').then(m => m.ViewSolicitacaoComponent) },
+<<<<<<< Updated upstream
   { path: 'view-maintenance', loadComponent: () => import('./pages/employer/view-maintenance/view-maintenance.component').then(m => m.ViewMaintenanceComponent) },
   { path: 'view-equipment-category', component: ViewEquipmentCategoryComponent },
+=======
+  { path: 'funcionario/home', loadComponent: () => import('./pages/employee/home-funcionario/home-funcionario.component').then(m => m.HomeFuncionarioComponent), canActivate: [authGuard], data: { role: 'funcionario' } },
+  { path: 'funcionario/home/efetuar-orcamento/:id', loadComponent: () => import('./pages/employee/efetuar-orcamento/efetuar-orcamento.component').then(m => m.EfetuarOrcamentoComponent), canActivate: [authGuard], data: { role: 'funcionario' } },
+  { path: 'funcionario/home/efetuar-manutencao/:id', loadComponent: () => import('./pages/employee/efetuar-manutencao/efetuar-manutencao.component').then(m => m.EfetuarManutencaoComponent), canActivate: [authGuard], data: { role: 'funcionario' } },
+  { path: 'funcionario/home/visualizacao-solicitacoes', loadComponent: () => import('./pages/employee/visualizacao-solicitacoes/visualizacao-solicitacoes.component').then(m => m.VisualizacaoSolicitacoesComponent), canActivate: [authGuard], data: { role: 'funcionario' } },
+  { path: 'funcionario/home/crud-funcionario', loadComponent: () => import('./pages/employee/crud-funcionario/crud-funcionario.component').then(m => m.CrudFuncionarioComponent), canActivate: [authGuard], data: { role: 'funcionario' } },
+  { path: 'funcionario/home/crud-categoria', loadComponent: () => import('./pages/employee/crud-categoria/crud-categoria.component').then(m => m.CrudCategoriaComponent), canActivate: [authGuard], data: { role: 'funcionario' } },
+  { path: 'funcionario/home/relatorio-receitas', loadComponent: () => import('./pages/employee/relatorio-receitas/relatorio-receitas.component').then(m => m.RelatorioReceitasComponent), canActivate: [authGuard], data: { role: 'funcionario' } },
+  { path: 'funcionario/home/relatorio-categorias', loadComponent: () => import('./pages/employee/relatorio-categorias/relatorio-categorias.component').then(m => m.RelatorioCategoriasComponent), canActivate: [authGuard], data: { role: 'funcionario' } },
+>>>>>>> Stashed changes
   { path: '**', redirectTo: '' }
 ];
