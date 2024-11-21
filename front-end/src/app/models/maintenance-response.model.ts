bf95bf6ebb0eb0b8455008cf968807e0c5bf52c1
Bruno_Brug <brunobrugnerottodelara@gmail.com>
2024-11-21 14:00:02 -0300

    [ADD] integração maintenance

---
+++ front-end/src/app/models/maintenance-response.model.ts
@@ -0,0 +1,7 @@
+export interface IMaintenanceResponse {
+    id: number;
+    descricao_equipamento: string;
+    descricao_defeito: string;
+    dataCriacao: string;
+    data_finalizacao: string;
+}
