5962ce0db7da21349dd1372f44e8aa0f4bace311
joao pedro guerios <joaopedroguerios@gmail.com>
2024-11-20 23:13:33 -0300

    melhoria mensagens de erro no autocadastro

--- front-end/src/app/material/form-register/form-register.component.ts
+++ front-end/src/app/material/form-register/form-register.component.ts
@@ -29,7 +29,8 @@ import { ICliente } from '../../services/api/customer.service'; // Ajuste o cami
 })
 export class FormRegisterComponent {
   registerForm: FormGroup;
-
+  statusMessage: string = ''; 
+  isSuccess: boolean = false; 
   constructor(private fb: FormBuilder, private router: Router, private customerService: CustomerService) {
     this.registerForm = this.fb.group({
       name: [
@@ -123,9 +124,9 @@ export class FormRegisterComponent {
       const cliente: ICliente = {
         nome: formValues.name,
         email: formValues.email,
-        cpf: formValues.cpf.replace(/\D/g, ''), 
+        cpf: formValues.cpf.replace(/\D/g, ''),
         telefone: formValues.phone.replace(/\D/g, ''),
-        cep: formValues.cep.replace(/\D/g, ''), 
+        cep: formValues.cep.replace(/\D/g, ''),
         pais: formValues.country,
         estado: formValues.state,
         cidade: formValues.city,
@@ -133,19 +134,22 @@ export class FormRegisterComponent {
         numero: formValues.numero,
         complemento: formValues.complemento || '',
       };
-  
+
       this.customerService.cadastrarCliente(cliente).subscribe(
         (response) => {
-          console.log('Cliente cadastrado com sucesso:', response);
-          this.goToLogin(); 
+          this.statusMessage = 'Cliente cadastrado com sucesso!';
+          this.isSuccess = true;
+          this.goToLogin();
         },
         (error) => {
-          console.log('Erro ao cadastrar cliente:', error); 
+          this.statusMessage = error;
+          this.isSuccess = false; 
         }
       );
     } else {
-      console.log('Formulário inválido');
       this.registerForm.markAllAsTouched();
+      this.statusMessage = 'Por favor, preencha todos os campos corretamente.';
+      this.isSuccess = false;
     }
   }
 }
