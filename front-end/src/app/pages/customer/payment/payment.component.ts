3def6dcb137285eaea9962bbc318d20b56600e32
joao pedro guerios <joaopedroguerios@gmail.com>
2024-12-09 23:25:26 -0300

    pagamento

--- front-end/src/app/pages/customer/payment/payment.component.ts
+++ front-end/src/app/pages/customer/payment/payment.component.ts
@@ -1,35 +1,73 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { ServicoStorageService } from '../../../services/servico-storage.service';
+import { ActivatedRoute, Router } from '@angular/router';
 import { HeaderClienteComponent } from '../../../material/header-cliente/header-cliente.component';
+import { CommonModule, DatePipe } from '@angular/common';
+import { BudgetService } from '../../../services/api/budget.service';
+import {
+  MaintenanceService,
+  MaintenanceResponse,
+} from '../../../services/api/maintenance.service';
+import { IBudgetResponse } from '../../../models/budget-response.model';
+
 @Component({
   selector: 'app-payment',
   standalone: true,
   imports: [HeaderClienteComponent, CommonModule],
+  providers: [DatePipe],
   templateUrl: './payment.component.html',
-  styleUrl: './payment.component.css'
+  styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent {
-  constructor(private servicoStorage: ServicoStorageService, private route: ActivatedRoute, private router: Router) { }
-  servicos: any[] = [];
-  item: any;
+  id: number | null = null;
+  erro: string | null = null;
+  servico: MaintenanceResponse | undefined;
+  budget: IBudgetResponse | undefined;
+
+  constructor(
+    private maintenanceService: MaintenanceService,
+    private budgetService: BudgetService,
+    private route: ActivatedRoute,
+    private datePipe: DatePipe,
+    private router: Router
+  ) {}
+
   ngOnInit() {
-    this.servicos = this.servicoStorage.getServicos();
-    const id = this.route.snapshot.paramMap.get('id');
-    const escolhido = this.servicos.filter(s => s.id === id);
-    this.item = escolhido[0];
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+    if (this.id) {
+      this.maintenanceService.getMaintenanceRecordById(this.id).subscribe({
+        next: (data) => {
+          this.servico = {
+            ...data,
+            dataConserto: this.datePipe.transform(data.dataConserto, 'dd/MM/yyyy') || undefined,
+            dataCriacao: this.datePipe.transform(data.dataCriacao, 'dd/MM/yyyy') || undefined,
+            dataFinalizacao: this.datePipe.transform(data.dataFinalizacao, 'dd/MM/yyyy') || undefined,
+          };
+        },
+        error: (error) => {
+          this.erro = error.message;
+          console.log('Erro ao carregar solicitação', error);
+        },
+      });
+    }
   }
+
   efetuarPagamento() {
-    const confirmacao = window.confirm(`Confirmar pagamento no valor de R$ ${this.item.preco}`);
-    if (confirmacao) {
-      const dadosAtualizados = {
-        status: 'PAGA',
-      };
-      window.alert('Serviço pago com sucesso!');
-      this.servicoStorage.updateServico(this.item.id, dadosAtualizados);
-      this.router.navigate(['/cliente/home']);
+    if (this.servico && this.budget) {
+      const confirmacao = window.confirm(`Confirmar pagamento no valor de R$ ${this.budget.precoOrcado}`);
+      if (confirmacao) {
+        this.maintenanceService.payMaintenance(this.servico.id!).subscribe({
+          next: () => {
+            window.alert('Serviço pago com sucesso!');
+            this.router.navigate(['/cliente/home']);
+          },
+          error: (error) => {
+            window.alert('Erro ao efetuar o pagamento.');
+            console.error('Erro no pagamento', error);
+          },
+        });
+      }
+    } else {
+      window.alert('Serviço ou orçamento não encontrado.');
     }
   }
 }
