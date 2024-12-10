7b838fa532cc6331687bdcbdb0c58eb09920bb4b
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2024-12-10 17:59:16 -0300

    add: payment modal

--- front-end/src/app/pages/customer/payment/payment.component.ts
+++ front-end/src/app/pages/customer/payment/payment.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HeaderClienteComponent } from '../../../material/header-cliente/header-cliente.component';
-import { CommonModule, DatePipe, CurrencyPipe  } from '@angular/common';
+import { CommonModule, DatePipe, CurrencyPipe } from '@angular/common';
 import { BudgetService } from '../../../services/api/budget.service';
 import {
   MaintenanceService,
@@ -15,7 +15,7 @@ import { IBudgetResponse } from '../../../models/budget-response.model';
   imports: [HeaderClienteComponent, CommonModule],
   providers: [DatePipe, CurrencyPipe],
   templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.css']
+  styleUrls: ['./payment.component.css'],
 })
 export class PaymentComponent {
   id: number | null = null;
@@ -23,7 +23,8 @@ export class PaymentComponent {
   servico: MaintenanceResponse | undefined;
   budget: any;
   mostrarInformacoesAtuais: boolean = true;
-  valorAlternado: string = "Informações do Conserto";
+  valorAlternado: string = 'Informações do Conserto';
+  paymentModal: Boolean = false;
 
   constructor(
     private maintenanceService: MaintenanceService,
@@ -35,34 +36,47 @@ export class PaymentComponent {
   ) {}
   ngOnInit() {
     this.id = Number(this.route.snapshot.paramMap.get('id'));
-    if (this.id !== null) {  // Verifique se id não é null
+    if (this.id !== null) {
+      // Verifique se id não é null
       this.budgetService.getBudgetByMaintenanceId(this.id).subscribe({
         next: (data) => {
           this.budget = {
             ...data,
-            dataRecuperacao: this.datePipe.transform(data.dataRecuperacao, 'dd/MM/yyyy') || undefined,
-            dataOrcamento: this.datePipe.transform(data.dataOrcamento, 'dd/MM/yyyy') || undefined,
-            precoOrcado: this.currencyPipe.transform(data.precoOrcado, 'BRL', 'symbol') || undefined,
-          };  
+            dataRecuperacao:
+              this.datePipe.transform(data.dataRecuperacao, 'dd/MM/yyyy') ||
+              undefined,
+            dataOrcamento:
+              this.datePipe.transform(data.dataOrcamento, 'dd/MM/yyyy') ||
+              undefined,
+            precoOrcado:
+              this.currencyPipe.transform(data.precoOrcado, 'BRL', 'symbol') ||
+              undefined,
+          };
         },
         error: (error) => {
           this.erro = error.message;
           console.log('Erro ao carregar orçamento', error);
-        }
+        },
       });
     } else {
       console.error('ID inválido');
     }
-  
+
     if (this.id) {
       this.maintenanceService.getMaintenanceRecordById(this.id).subscribe({
         next: (data) => {
-          console.log(data)
+          console.log(data);
           this.servico = {
             ...data,
-            dataConserto: this.datePipe.transform(data.dataConserto, 'dd/MM/yyyy') || undefined,
-            dataCriacao: this.datePipe.transform(data.dataCriacao, 'dd/MM/yyyy') || undefined,
-            dataFinalizacao: this.datePipe.transform(data.dataFinalizacao, 'dd/MM/yyyy') || undefined,
+            dataConserto:
+              this.datePipe.transform(data.dataConserto, 'dd/MM/yyyy') ||
+              undefined,
+            dataCriacao:
+              this.datePipe.transform(data.dataCriacao, 'dd/MM/yyyy') ||
+              undefined,
+            dataFinalizacao:
+              this.datePipe.transform(data.dataFinalizacao, 'dd/MM/yyyy') ||
+              undefined,
           };
         },
         error: (error) => {
@@ -72,28 +86,36 @@ export class PaymentComponent {
       });
     }
   }
+
   alternarInformacoes() {
     this.mostrarInformacoesAtuais = !this.mostrarInformacoesAtuais;
-    this.valorAlternado = this.mostrarInformacoesAtuais  ? "Informações do Conserto" : "Informações da solicitação"; 
+    this.valorAlternado = this.mostrarInformacoesAtuais
+      ? 'Informações do Conserto'
+      : 'Informações da solicitação';
+  }
+
+  openPaymentModal() {
+    this.paymentModal = true;
+  }
+
+  closePaymentModal() {
+    this.paymentModal = false;
   }
 
   efetuarPagamento() {
     if (this.servico && this.budget) {
-      const confirmacao = window.confirm(`Confirmar pagamento no valor de R$ ${this.budget.precoOrcado}`);
-      if (confirmacao) {
-        this.maintenanceService.payMaintenance(this.servico.id!).subscribe({
-          next: () => {
-            window.alert('Serviço pago com sucesso!');
-            this.router.navigate(['/cliente/home']);
-          },
-          error: (error) => {
-            window.alert('Erro ao efetuar o pagamento.');
-            console.error('Erro no pagamento', error);
-          },
-        });
-      }
+      this.maintenanceService.payMaintenance(this.servico.id!).subscribe({
+        next: () => {
+          this.router.navigate(['/cliente/home']);
+        },
+        error: (error) => {
+          window.alert('Erro ao efetuar o pagamento.');
+          console.error('Erro no pagamento', error);
+        },
+      });
     } else {
       window.alert('Serviço ou orçamento não encontrado.');
     }
+    this.closePaymentModal();
   }
 }
